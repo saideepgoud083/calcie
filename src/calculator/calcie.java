dd46209f300b588c83b023909f00debadcd67a30
saideepgoud083 <saideepsaideep083@gmail.com>
2025-11-15 19:16:48 +0530

    this is initial commit

---
+++ src/calculator/calcie.java
@@ -0,0 +1,13 @@
+package calculator;
+
+import java.util.List;
+
+public interface calcie {
+
+	
+	public int addition(int a,int b);
+	public double sub(int a,int b);
+	public int mul(int a,int b);
+	public int div(int a,int b);
+	
+}
