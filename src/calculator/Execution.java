2b9084c7354f620c7385dfab5e82e59fb7a259f2
saideepgoud083 <saideepsaideep083@gmail.com>
2025-11-18 20:01:00 +0530

    THIS IS NEW FATUTR'

---
+++ src/calculator/Execution.java
@@ -0,0 +1,62 @@
+
+package calculator;
+
+import java.util.Scanner;
+
+public class Execution {
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter your operation");
+		System.out.println("1:Addtion");
+		System.out.println("2:Subration");
+		System.out.println("3:Multiplication");
+		System.out.println("4:Division");
+		System.out.println("5:Exit");
+		int op = sc.nextInt();
+		boolean flag = true;
+		while(flag){
+			
+			switch(op){
+			case 1 :{
+				
+				
+			}break;
+	        case 2 :{
+				
+				
+			}break;
+	       case 3 :{
+				
+				
+			}break;
+	       case 4 :{
+				
+				
+			}break;
+			
+	       case 5 :{
+	    	   
+				flag=false;
+				
+			}break;
+			
+			
+			
+			
+		}
+		
+		
+		
+			
+		}
+		
+		
+		
+		
+	}
+
+}
